dd3b1d3fef0841b48ada80b52ec90cadf6570f2c
Shapoval <shapoval.sergey@mail.ru>
2019-11-28 23:54:38 +0700

    Google ok

---
+++ google_search.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://www.google.com/")
+time.sleep(5)
+textinput = driver.find_element_by_name("q")
+textinput.send_keys("Журенков")
+submit_button = driver.find_element_by_name("btnK")
+submit_button.click()
+time.sleep(10)
+driver.quit()
